56d41a04019946327e40ff75b18a8cbb74a8708a
Mauro Avellaneda <mauroeavellaneda@gmail.com>
2021-02-06 18:29:22 -0300

    Creates data folder to contain info for fetching purposes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React from "react";
+import { links, social } from "../data/links";
+import { FaBars, FaTwitter } from "react-icons/fa";
 
 const Navbar = () => {
-    return (
-        <div>
-            
+  return (
+    <>
+      <nav className="nav-center">
+        <div className="nav-header">
+          <h1>Cancionero Popular</h1>
+          <button className="nav-toggle">
+            <FaBars />
+          </button>
         </div>
-    )
-}
+        <div className="links-container show-container">
+          <ul className="links">
+            {links.map((link) => {
+              const { id, url, text } = link;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <ul className="social-icons">
+          {social.map((socialIcon) => {
+            const {id,url,icon} = socialIcon;
+            return <li key={id}>
+              <a href={url}>{icon}</a>
+            </li>
 
-export default Navbar
+          })}
+        </ul>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
