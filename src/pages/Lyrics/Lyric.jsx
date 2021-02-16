e2fa52fba36ea690366dc08cc0ef3348ef6e21cb
Mauro Avellaneda <mauroeavellaneda@gmail.com>
2021-02-16 08:31:05 +0100

    Refactoring

--- src/pages/Lyrics/Lyric.jsx
+++ src/pages/Lyrics/Lyric.jsx
@@ -1,33 +1,58 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./Lyric.css";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import CardActions from "@material-ui/core/CardActions";
+import Button from "@material-ui/core/Button";
+import { Divider, makeStyles } from "@material-ui/core";
+import CardHeader from "@material-ui/core/CardHeader";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    borderRadius: 12,
+    minWidth: 256,
+    textAlign: "center",
+  },
+  header: {
+    textAlign: "center",
+    spacing: 10,
+  },
+  list: {
+    padding: "20px",
+  },
+  button: {
+    margin: theme.spacing(1),
+  },
+  action: {
+    display: "flex",
+    justifyContent: "space-around",
+  },
+}));
 
 const Lyric = ({ track }) => {
+  const classes = useStyles();
+
   return (
-    <div className="">
-      <div className="">
-        <div className="">
-          <h5>{track.artist_name}</h5>
-          <p className="">
-            <strong>
-              <i className="">Canción</i>
-            </strong>
-            : {track.track_name}
-            <br />
-            <strong>
-              <i className=""></i>Album
-            </strong>
-            : {track.album_name}
-          </p>
-          <Link
-            to={`lyrics/track/${track.track_id}`}
-            className=""
-          >
-            <i className=""></i> Ver Letra
-          </Link>
+    <Card className={classes.root}>
+      <CardHeader title={track.artist_name} className={classes.header} />
+      <Divider variant="middle" />
+      <CardContent>
+        <Typography variant="h5" align="center">
+          {track.track_name}
+        </Typography>
+        <div className={classes.list}>
+          <Typography align="center">Canción: {track.track_name}</Typography>
+          <Typography align="center">Álbum: {track.album_name}</Typography>
         </div>
-      </div>
-    </div>
+      </CardContent>
+      <Divider variant="middle" />
+      <CardActions className={classes.action}>
+        <Button variant="contained" color="primary" className={classes.button}>
+          <Link to={`lyrics/track/${track.track_id}`}>Ver Letra</Link>
+        </Button>
+      </CardActions>
+    </Card>
   );
 };
 
